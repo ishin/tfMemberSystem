6f92dd0b462a6b3b633644240f0617164041a6ba
LianMengYu <lian4040>
2017-06-12 18:33:46 +0800

    1.重新提交

--- android/TianFangIMS/app/src/main/java/com/tianfangIMS/im/utils/http.java
+++
@@ -1,8 +0,0 @@
-package com.tianfangIMS.im.utils;
-
-/**
- * Created by LianMengYu on 2017/1/7.
- */
-
-public class http {
-}
--- android/TianFangIMS/ptt/src/main/java/io/rong/ptt/message/InfoNotificationMsgInterface.java
+++
@@ -1,9 +0,0 @@
-package io.rong.ptt.message;
-
-/**
- * Created by jiangecho on 2017/1/13.
- */
-
-public interface InfoNotificationMsgInterface {
-    String getMessage();
-}
