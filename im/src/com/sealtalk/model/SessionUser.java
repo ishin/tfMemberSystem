173a00f5e3b8f30c02824a068d9daa163d966257
hao_dy <hao_dy@pataw.cn>
2017-06-19 17:30:33 +0800

    修改

--- im/src/com/sealtalk/model/SessionUser.java
+++ im/src/com/sealtalk/model/SessionUser.java
@@ -1,21 +1,29 @@
 package com.sealtalk.model;
 
-/** 
-* @ClassName: SessionUser 
-* @Description: TODO(代表已登录用户的包装类,表示此用户处于会话中。) 
-* @author hdy
-*  
-*/
-public class SessionUser
-{
-	/** 
-	* @Fields accountId : TODO(账号ID) 
-	*/ 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @ClassName: SessionUser
+ * @Description: TODO(代表已登录用户的包装类,表示此用户处于会话中。)
+ * @author hdy
+ * 
+ */
+public class SessionUser {
+	/**
+	 * @Fields accountId : TODO(账号ID)
+	 */
 	private int id;
-	private String account; 
+	private String account;
 	private String fullname;
 	private String token;
 	private int organId;
+	private Map<String, Long> apMap;		//用来做同接口请求频率限制
+
+	public SessionUser() {
+		super();
+		apMap = new HashMap<String, Long>();
+	}
 	
 	public int getId() {
 		return id;
@@ -56,5 +64,12 @@ public class SessionUser
 	public void setOrganId(int organId) {
 		this.organId = organId;
 	}
-	
+
+	public Map<String, Long> getApMap() {
+		return apMap;
+	}
+
+	public void setApMap(Map<String, Long> apMap) {
+		this.apMap = apMap;
+	}
 }
